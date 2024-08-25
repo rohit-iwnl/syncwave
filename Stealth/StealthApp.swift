b956463fb5d16b01f86f72abc0429c2ad4071c38
rohit-iwnl <rohitmanivel9@gmail.com>
2024-08-25 16:44:48 -0700

    Initial Commit

---
+++ Stealth/StealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  StealthApp.swift
+//  Stealth
+//
+//  Created by Rohit Manivel on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct StealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
