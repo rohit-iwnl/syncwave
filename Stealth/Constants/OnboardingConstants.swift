bf4c2cd4f2c6563c2c54a9d5d52d15ac3122e43a
rohit-iwnl <rohitmanivel9@gmail.com>
2024-08-25 16:56:48 -0700

    Initial commit

---
+++ Stealth/Constants/OnboardingConstants.swift
@@ -0,0 +1,39 @@
+//
+//  OnboardingConstants.swift
+//  Stealth
+//
+//  Created by Rohit Manivel on 8/25/24.
+//
+import Foundation
+import SwiftUI
+
+struct OnboardingPage {
+    let color : Color
+    let image : String
+    let title : String
+    let desciption: String
+}
+
+
+struct OnboardingConstants{
+    static let pages : [OnboardingPage] = [
+        OnboardingPage(
+            color: Color(hex: "#CDE3F3"),
+            image: "circles",
+            title: "Welcome to Stealth",
+            description: "Your all-in-one solution for student life, Find accommodation, Organize schedules, simplify tasks and many more."
+        ),
+        OnboardingPage(
+            color: Color(hex: "#F8EED5"),
+            image: "star",
+            title: "Find your Perfect Place",
+            description: "Discover tailored accommodations within budget. From cozy studios to shared spaces along with your preferred roommate."
+        ),
+        OnboardingPage(
+            color: Color(hex: "#D4ECCD"),
+            image: "aistar",
+            title: "Simplify your life",
+            description: "Manage schedules, set reminders, access resources. Let’s make your journey easier."
+        )
+    ]
+}
