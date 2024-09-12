13a2d7156a7ade7c4714cae840d487703b797695
rohit-iwnl <rohitmanivel9@gmail.com>
2024-09-12 15:10:35 -0700

    add custom sliders

--- Stealth/Core/Views/Preferences/Components/CustomSlider.swift
+++ Stealth/Core/Views/Preferences/Components/CustomSlider.swift
@@ -6,13 +6,40 @@
 //
 
 import SwiftUI
+import Sliders
 
-struct CustomSlider: View {
+
+struct RentRangeSlider: View {
+    @State var range = 0.2...0.8
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        RangeSlider(range: $range)
+            .rangeSliderStyle(
+                HorizontalRangeSliderStyle(
+                    track: HorizontalRangeTrack(
+                        view: Capsule().foregroundColor(.black)
+                    )
+                    .background(Capsule().foregroundColor(.gray))
+                    .frame(height: 6),
+                    lowerThumb: RoundedRectangle(cornerRadius: 2).foregroundStyle(.white),
+                    
+                    upperThumb:  RoundedRectangle(cornerRadius: 2).foregroundStyle(.white),
+                    lowerThumbSize: CGSize(width: 20, height: 20),
+                    upperThumbSize: CGSize(width: 20, height: 20)
+                )
+            )
+            .padding()
     }
 }
 
+
+
 #Preview {
-    CustomSlider()
+    VStack{
+        Spacer()
+        RentRangeSlider()
+        Spacer()
+    }
+    .background(.gray.opacity(0.5))
+    
 }
