cb882c5d65a87a41aae66c2ef9b3b9c447049b4e
rohit-iwnl <rohitmanivel9@gmail.com>
2024-09-13 16:52:51 -0700

    add models for range sliders and other options in housing options

--- Stealth/Core/Models/Preferences/Housing/HousingModels.swift
+++ Stealth/Core/Models/Preferences/Housing/HousingModels.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+struct RentRangeSlider {
+    var min : Double
+    var max : Double
+}
+
+struct propertySizeSlider {
+    var min : Double
+    var max : Double
+}
+
+struct BedroomsOptions {
+    static let options : [String] = ["1 BR", "2 BR", "3 BR", "4+ BR", "Any"]
+}
+
+struct BathroomOptions {
+    static let options : [String] = ["1", "2", "3", "4+", "Any"]
+}
+
+struct RoomateOptions {
+    static let options : [String] = ["1", "2", "3", "4+", "Any"]
+}
+
+struct FurnishingOptions {
+    static let options : [String] = ["Fully Furnished", "Semi Furnished", "Any"]
+}
+
+struct AmenitiesOptions {
+    static let options: [String] = [
+        "Bicycle Parking",
+        "Swimming Pool",
+        "Covered Parking",
+        "Open Parking",
+        "Smoking Area",
+        "In-House Laundry",
+        "Workspace",
+        "Recreational Area",
+        "Accessible",
+        "Gym"
+    ]
+}
+
