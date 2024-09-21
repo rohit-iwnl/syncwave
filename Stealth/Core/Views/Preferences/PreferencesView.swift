751e34b3a1a0a941bc53773f7f5fa74b5baaee89
rohit-iwnl <rohitmanivel9@gmail.com>
2024-09-21 02:49:21 -0700

    use navigation path in prefs view

--- Stealth/Core/Views/Preferences/PreferencesView.swift
+++ Stealth/Core/Views/Preferences/PreferencesView.swift
@@ -6,43 +6,70 @@
 //
 
 import SwiftUI
+import NavigationTransitions
 
 struct PreferencesView: View {
-    @State private var currentPage = 0
-    @State private var totalPages = 3
-    @StateObject private var keychainVM = KeychainViewModel() // Inject KeychainViewModel
+    @StateObject private var keychainVM = KeychainViewModel()
+    @State private var isShowingHousingPreferences: Bool = false
+    @State private var showPages: Bool = true
+    @StateObject var navigationCoordinator = NavigationCoordinator()
+    @Environment(\.dismiss) private var dismiss
     
-    @State private var keychainValue: String = "" // State to hold the fetched value
-    
-    @State private var isShowingHousingPreferences : Bool = false
+    @State private var preferencesArray : [String : Bool] = [:]
     
     var body: some View {
         VStack(spacing: 0) {
-            PreferencesToolbar(currentPage: $currentPage, totalPages: $totalPages, showSkipButton: $isShowingHousingPreferences)
+            PreferencesToolbar(
+                currentPage : $navigationCoordinator.currentPage,
+                totalPages: $navigationCoordinator.totalPages,
+                showSkipButton: $isShowingHousingPreferences,
+                showPages: $navigationCoordinator.showPages,
+                onBackTap: handleBackTap
+            )
             
-            ZStack {
-                
-                Group{
-                    OptionsView(currentPage: $currentPage, totalPages: $totalPages)
-                        .opacity(currentPage == 0 ? 1 : 0)
-                        .animation(.easeInOut, value: currentPage)
-                    
-                    PersonalInfoView(currentPage: $currentPage)
-                        .opacity(currentPage == 1 ? 1 : 0)
-                        .animation(.easeInOut, value: currentPage)
-                    
-                    // Third screen with Keychain fetch
-                    HousingPreferencesView(currentPage: $currentPage, totalPages: $totalPages, isShowingHousingPreferences: $isShowingHousingPreferences)
-                        .opacity(currentPage == 2 ? 1 : 0)
-                        .animation(.easeInOut, value: currentPage)
+            NavigationStack(path: $navigationCoordinator.path) {
+                Group {
+                    if navigationCoordinator.path.isEmpty {
+                        OptionsView(currentPage: $navigationCoordinator.currentPage, totalPages: $navigationCoordinator.totalPages, preferencesArray: $preferencesArray)
+                    }
+                }
+                .navigationDestination(for: String.self) { destination in
+                    switch destination {
+                    case "PersonalInfo":
+                        PersonalInfoView(currentPage: $navigationCoordinator.currentPage, preferencesArray: $preferencesArray)
+                            .toolbar(.hidden)
+                            .transition(.opacity)
+                            
+                    default:
+                        Text("Unknown destination: \(destination)")
+                    }
                 }
             }
+            .environmentObject(navigationCoordinator)
+            
+            
         }
-        .toolbar(.hidden)
+        .navigationBarHidden(true)
+    }
+    
+    private func handleBackTap() {
+        withAnimation(.easeInOut(duration: 0.5)) {
+            if !navigationCoordinator.path.isEmpty {
+                if navigationCoordinator.path.count == 1 {
+                    navigationCoordinator.showPages = false
+                }
+                navigationCoordinator.path.removeLast()
+                navigationCoordinator.currentPage = navigationCoordinator.path.count
+                
+            } else {
+                dismiss()
+            }
+        }
     }
 }
 
+
 #Preview {
     PreferencesView()
+        .environmentObject(NavigationCoordinator())
 }
-
