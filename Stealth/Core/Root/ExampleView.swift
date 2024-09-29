bd7558e58a436f59b14f91c84a371a401a513542
rohit-iwnl <rohitmanivel9@gmail.com>
2024-09-29 01:34:21 -0700

    add nav stack to content view

---
+++ Stealth/Core/Root/ExampleView.swift
@@ -0,0 +1,18 @@
+//
+//  ExampleView.swift
+//  Stealth
+//
+//  Created by Rohit Manivel on 9/28/24.
+//
+
+import SwiftUI
+
+struct ExampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExampleView()
+}
