d8a00475cffbe92c52d4ecd4cb1edb2099d1f82e
rohit-iwnl <rohitmanivel9@gmail.com>
2024-10-01 14:34:39 -0700

    add enum for navigation destinations

---
+++ Stealth/Enums/NavigationDestinations.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationDestinations.swift
+//  Stealth
+//
+//  Created by Rohit Manivel on 10/1/24.
+//
+
+import Foundation
+
+enum NavigationDestinations : String, Hashable {
+    case home = "Home"
+    case settings = "Settings"
+    case personalInfo = "PersonalInfo"
+    case preferences = "Preferences"
+    case welcome = "Welcome"
+    case registerAccount = "registerAccount"
+}
