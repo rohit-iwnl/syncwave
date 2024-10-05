0378d81bd008a0c37355132734ab83c41e565da4
rohit-iwnl <rohitmanivel9@gmail.com>
2024-10-04 20:31:07 -0700

    add loading view

--- Stealth/Core/Views/Loading/LoadingView.swift
+++ Stealth/Core/Views/Loading/LoadingView.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct LoadingView: View {
+    @State private var isGlowing = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            Image("Logo/white") // Ensure "appicon" matches your asset name
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .scaledToFit()
+                .frame(width: 150, height: 150)
+                .glow(color: Color(hex: "#C3FF19"), radius: isGlowing ? 10 : 0)
+                .animation(
+                    Animation.easeInOut(duration: 1)
+                        .repeatForever(autoreverses: true),
+                    value: isGlowing
+                )
+            Spacer()
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(TextColors.primaryBlack.color)
+        .onAppear {
+            isGlowing.toggle() // Start the glowing animation
+        }
+    }
+}
+
+struct GlowEffect: ViewModifier {
+    var color: Color
+    var radius: CGFloat = 20
+    
+    func body(content: Content) -> some View {
+        content
+            .shadow(color: color.opacity(0.5), radius: radius, x: 0, y: 0)
+            .shadow(color: color.opacity(0.5), radius: radius, x: 0, y: 0)
+            .shadow(color: color.opacity(0.5), radius: radius, x: 0, y: 0)
+    }
+}
+
+// Extension to apply the glow effect easily
+extension View {
+    func glow(color : Color, radius: CGFloat = 20) -> some View {
+        self.modifier(GlowEffect(color: color, radius: radius))
     }
 }
 
