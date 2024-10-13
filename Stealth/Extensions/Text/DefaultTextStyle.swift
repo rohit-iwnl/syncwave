027357b6e270c6b98b2bba41d92319d0ba140118
rohit-iwnl <rohitmanivel9@gmail.com>
2024-10-13 16:06:58 -0700

    add default sora font style

--- Stealth/Extensions/Text/DefaultTextStyle.swift
+++ Stealth/Extensions/Text/DefaultTextStyle.swift
@@ -5,4 +5,25 @@
 //  Created by Rohit Manivel on 10/13/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct DefaultTextStyle : ViewModifier {
+    
+    @Environment(\.dynamicTypeSize) var dynamicTypeSize
+    var style : Font.TextStyle
+    var weight: Font.Weight
+    
+    
+    func body(content : Content) -> some View {
+        content
+            .font(.sora(style, weight: weight))
+            .minimumScaleFactor(dynamicTypeSize.customMinScaleFactor)
+    }
+}
+
+extension View {
+    func soraStyle(style : Font.TextStyle = .body ,weight: Font.Weight = .regular) -> some View {
+        self.modifier(DefaultTextStyle(style: style, weight: weight))
+    }
+}
+
