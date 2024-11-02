bd05ac9e7c59278d201f7f780e5d72dc33c99dd4
rohit-iwnl <rohitmanivel9@gmail.com>
2024-11-01 22:21:12 -0700

    add illustrations

---
+++ Stealth/Core/Views/Preferences/Tabs/RoomDeciderView.swift
@@ -0,0 +1,18 @@
+//
+//  RoomDeciderView.swift
+//  Stealth
+//
+//  Created by Rohit Manivel on 11/1/24.
+//
+
+import SwiftUI
+
+struct RoomDeciderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoomDeciderView()
+}
