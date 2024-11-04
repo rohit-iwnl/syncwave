13041bef157898da6be55115f0a4c86dafc27cab
rohit-iwnl <rohitmanivel9@gmail.com>
2024-11-03 18:57:34 -0700

    add focus fields for inputs

--- Stealth/Utils/Inputs/LabeledInputButton.swift
+++ Stealth/Utils/Inputs/LabeledInputButton.swift
@@ -7,6 +7,13 @@
 
 import SwiftUI
 
+enum Field: Hashable {
+    case baseRent
+    case perPersonRent
+    case squareFootage
+    case generateDescription
+}
+
 struct LabeledInputButton: View {
     let label: String
     let placeholder: String
@@ -16,13 +23,14 @@ struct LabeledInputButton: View {
     @State private var showError = false
     @Environment(\.dynamicTypeSize) var dynamicTypeSize
     
-    
     var leftSideText: String = ""
     
+    // Change this to FocusState binding
+    @FocusState.Binding var focusedField: Field?
+    let field: Field
     
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
-            
             Text(label)
                 .font(.sora(.headline, weight: .regular))
                 .foregroundColor(.black)
@@ -37,9 +45,6 @@ struct LabeledInputButton: View {
                 Divider()
                     .frame(height: 20)
                     .padding(.horizontal, 8)
-                
-                
-                
                 
                 TextField(placeholder, text: Binding(
                     get: { value == 0 ? "" : "\(value)" },
@@ -59,9 +64,8 @@ struct LabeledInputButton: View {
                 ))
                 .font(.sora(.body, weight: .regular))
                 .keyboardType(keyboardType)
-
+                .focused($focusedField, equals: field)
             }
-            
             .padding(.vertical)
             .background(
                 RoundedRectangle(cornerRadius: 8)
@@ -74,14 +78,9 @@ struct LabeledInputButton: View {
                     .font(.caption)
             }
         }
-        .onAppear {
-            // Initialize textInput with current value if not zero
-            if value != 0 {
-                textInput = String(value)
-            }
-        }
     }
 }
+
 
 
 
@@ -89,18 +88,31 @@ struct LabeledInputButton: View {
 
 // Preview
 struct LabeledInputButtonPreview: View {
-    @State private var rentValue : Int = 0
+    @State private var rentValue: Int = 0
+    @FocusState private var focusedField: Field?
     
     var body: some View {
         LabeledInputButton(
             label: "Monthly base rent of the unit?",
             placeholder: "Enter your unit base rent",
             value: $rentValue,
-            keyboardType: .numberPad
+            keyboardType: .numberPad,
+            leftSideText: "$",
+            focusedField: $focusedField,
+            field: .baseRent
         )
         .padding()
+        .toolbar {
+            ToolbarItemGroup(placement: .keyboard) {
+                Spacer()
+                Button("Done") {
+                    focusedField = nil
+                }
+            }
+        }
     }
 }
+
 
 #Preview {
     LabeledInputButtonPreview()
