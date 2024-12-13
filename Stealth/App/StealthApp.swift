2611f791baf82665dffe6bfedf79c67a2575c24b
rohit-iwnl <rohitmanivel9@gmail.com>
2024-12-13 15:16:48 -0500

    animate the launch screen with opacity

--- Stealth/App/StealthApp.swift
+++ Stealth/App/StealthApp.swift
@@ -11,27 +11,31 @@ import SwiftUI
 @main
 struct StealthApp: App {
     @State private var isLaunchViewPresented = true
-
+    @State private var contentViewOpacity = 0.0 // Start with ContentView fully transparent
+    
     var body: some Scene {
         WindowGroup {
             ZStack {
+                ContentView()
+                    .opacity(contentViewOpacity)
+                    .onOpenURL { url in
+                        GIDSignIn.sharedInstance.handle(url)
+                    }
+                
                 if isLaunchViewPresented {
                     LaunchView()
                         .onAppear {
                             // Delay to allow the launch animation to complete
                             DispatchQueue.main.asyncAfter(deadline: .now() + 4.0) { // 4.0 seconds for the full animation
-                                withAnimation {
+                                withAnimation(.easeInOut(duration: 1.5)) {
                                     isLaunchViewPresented = false
+                                    contentViewOpacity = 1.0 // Fade in the ContentView
                                 }
                             }
-                        }
-                } else {
-                    ContentView()
-                        .onOpenURL { url in
-                            GIDSignIn.sharedInstance.handle(url)
                         }
                 }
             }
         }
     }
+    
 }
