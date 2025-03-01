edc87cc7dc10edd6f49524c1c5eff5736aa1c94a
rohit-iwnl <rohitmanivel9@gmail.com>
2025-02-28 23:46:09 -0700

    add single choice selector view

--- Stealth/Core/Components/Select/SingleChoiceSelector.swift
+++ Stealth/Core/Components/Select/SingleChoiceSelector.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
+struct SingleChoiceSelector: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    SwiftUIView()
+    SingleChoiceSelector()
 }
