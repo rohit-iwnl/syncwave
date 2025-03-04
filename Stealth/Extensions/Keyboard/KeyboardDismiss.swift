d8f8f7d231ca6ff8b26e342b5c0ac5c4266faa8b
rohit-iwnl <rohitmanivel9@gmail.com>
2025-03-03 22:37:15 -0700

    add keyboard dismissal extension with custom text

--- Stealth/Extensions/Keyboard/KeyboardDismiss.swift
+++ Stealth/Extensions/Keyboard/KeyboardDismiss.swift
@@ -8,11 +8,12 @@
 import SwiftUI
 
 extension View {
-    func addDismissButton() -> some View {
+    func addDismissButton(text : String = "Done") -> some View {
+        
         return self.toolbar {
             ToolbarItemGroup(placement: .keyboard) {
                 Spacer()
-                Button("Dismiss") {
+                Button(text) {
                     UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
                 }
                 .foregroundStyle(.black)
