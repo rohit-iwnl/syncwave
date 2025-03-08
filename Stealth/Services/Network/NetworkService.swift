17c47ede74ecff3aaedb08e28819c7d8c630079c
rohit-iwnl <rohitmanivel9@gmail.com>
2025-03-07 18:02:06 -0700

    add method to store user traits

--- Stealth/Services/Network/NetworkService.swift
+++ Stealth/Services/Network/NetworkService.swift
@@ -16,14 +16,13 @@ typealias NetworkResult<U: Codable> = (U) async throws -> Void
 
 struct EmptyResponse: Codable {}
 
-
 class NetworkService {
     static let shared = NetworkService()
     private let baseURL = "http://159.89.222.41:8000/api"
     private let authToken = "Bearer Naandhaandaungoppan"
-    
+
     private init() {}
-    
+
     func request<T: Codable, U: Codable>(
         endpoint: String,
         method: HTTPMethod,
@@ -32,12 +31,12 @@ class NetworkService {
         guard let url = URL(string: baseURL + endpoint) else {
             throw NetworkError.invalidURL
         }
-        
+
         var request = URLRequest(url: url)
         request.httpMethod = method.rawValue
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.setValue(authToken, forHTTPHeaderField: "Authorization")
-        
+
         if let body = body {
             do {
                 let jsonData = try JSONEncoder().encode(body)
@@ -49,34 +48,48 @@ class NetworkService {
                 throw NetworkError.encodingError
             }
         }
-        
+
         let (data, response) = try await URLSession.shared.data(for: request)
-        
+
         guard let httpResponse = response as? HTTPURLResponse else {
             throw NetworkError.invalidResponse
         }
-        
+
         print("Status code: \(httpResponse.statusCode)")
-        
+
         guard (200...299).contains(httpResponse.statusCode) else {
-            throw NetworkError.serverError("API call failed with status code: \(httpResponse.statusCode)")
+            throw NetworkError.serverError(
+                "API call failed with status code: \(httpResponse.statusCode)")
         }
-        
+
         do {
-                    let decodedResponse = try JSONDecoder().decode(U.self, from: data)
-                    return decodedResponse
-                } catch {
-                    throw NetworkError.decodingError
-                }
+            let decodedResponse = try JSONDecoder().decode(U.self, from: data)
+            return decodedResponse
+        } catch {
+            throw NetworkError.decodingError
+        }
     }
-    
-    func sendPreferences(_ preferences: [String: Bool], supabaseID: String) async throws {
-        let payload = PreferencesPayload(supabase_id: supabaseID, preferences: preferences)
-        
+
+    func sendPreferences(_ preferences: [String: Bool], supabaseID: String)
+        async throws
+    {
+        let payload = PreferencesPayload(
+            supabase_id: supabaseID, preferences: preferences)
+
         let _: EmptyResponse = try await request(
             endpoint: "/onboarding/set-preferences",
             method: .post,
             body: payload
+        )
+    }
+
+    /// Store user traits
+    func storeUserTraits(basicInfoSubmission payload: BasicInfoSubmission, supabaseID: String)
+        async throws
+    {
+
+        let _: EmptyResponse = try await request(
+            endpoint: "/personality-traits", method: .post, body: payload
         )
     }
 }
